6c7db34a7ed9beac268def66b13012d27a9da193
Yvan0112 <iryvan1@live.fr>
2020-02-10 17:25:17 +0100

    onepage et vitrine

---
+++ jour7/onepage/index.php
@@ -0,0 +1,9 @@
+<?php
+$licence = "tous droits réservés";
+$classbody="index";
+$titrepage="WELCOME TO YVAN'S WORLD";
+$nomAuteur="Yvan";
+require_once "php/view/header.php";
+require_once "php/view/section.php";
+require_once "php/view/footer.php";
+ ?>
---
+++ jour7/onepage/php/view/section.php
@@ -0,0 +1,31 @@
+<section id="s1">
+        <h2>Présentation</h2>
+        <p> IRAMBONA YVAN</p>
+        <p> 38 rue du Bon Pasteur</p>
+        <p> 13002 Marseille</p>
+        <p> iryvan1@live.fr</p>
+        <p> tél:0758213184 </p>
+        
+    </section>
+
+    <section id="s2">
+        <h2>Formations</h2>
+        <p>2020     : Développement Web/Mobile
+        <p>2015-2016: 1ère année Master Automatique et Système Electrique</p>
+        <p>2010-2015: Licence Electronique Electrotechnique Automatique </p>
+    </section>
+
+    <section id="s3">
+        <h2>Expériencces professionnelles</h2>
+        <p>2017-2019: SOCAMIL,E.LECLERC</p>
+    </section>
+
+    <section id="s4">
+        <h2>Compétences</h2>
+        <p>Lorem ipsres in eum modi vel culpa quo repellendus, iste eligendi fuga aspernatur, veniam sapiente, ducimus saepe delectus! Similique harum unde est.</p>
+    </section>
+
+    <section id="s5">
+        <h2>Loisirs</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi maiores in eum modi vel culpa quo repellendus, iste eligendi fuga aspernatur, veniam sapiente, ducimus saepe delectus! Similique harum unde est.</p>
+    </section>
